1758c1a390882dae8b7b91cbc967146b24b3aa3d
Shahmir Minhas <62132242+shahmir-minhas@users.noreply.github.com>
2021-12-24 03:06:16 -0800

    multiple state one reducer

--- index.js
+++ index.js
@@ -35,3 +35,52 @@ console.log('intinal State', store.getState());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 console.log(store.getState());
+
+
+// =========================================================================
+// ==================   use single reducer for multiple action    =======================
+// =========================================================================
+
+const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "buying Cake",
+  };
+}
+
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+    info: "buying icecream",
+  };
+}
+
+const initailState ={ 
+    numOfCake : 10,
+    numOfIceCream: 20,
+}
+
+const reducer = (state = initailState, action)=>{
+    if(action.type === 'BUY_CAKE'){
+
+        return {...state, numOfCake : state.numOfCake + 1}
+    }
+    if(action.type === BUY_ICECREAM){
+
+        return {...state, numOfIceCream : state.numOfIceCream + 1}
+    }
+}
+
+const redux = require('redux');
+const store = redux.createStore(reducer);
+
+console.log('intinal State', store.getState());
+const unsubscribe = store.subscribe(()=>{console.log('upadate State', store.getState())});
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+unsubscribe();
