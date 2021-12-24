d0540d665294d6fe83860d43704baf121e582be7
Shahmir Minhas <62132242+shahmir-minhas@users.noreply.github.com>
2021-12-24 03:43:24 -0800

    applied middleware (logger)

--- index.js
+++ index.js
@@ -138,13 +138,92 @@ const rootReducer = redux.combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
 });
-
+//  pass both reducers as an object
+// const rootReducer = combineReducer({
+//   cake: cakeReducer,
+//   iceCream: iceCreamReducer,
+// });
 //  pass rootreducer in create store
 const store = redux.createStore(rootReducer);
 
 console.log("intinal State", store.getState());
 const unsubscribe = store.subscribe(() => {
   console.log("upadate State", store.getState());
+});
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+unsubscribe();
+
+// =========================================================================
+// ==================  Redux Logger (MIDDLEWARE (THIRD PARTY)    ============
+// =========================================================================
+
+// -------------    actions
+const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
+
+// -------------    actions creators
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "buying Cake",
+  };
+}
+
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+    info: "buying icecream",
+  };
+}
+
+// -------------    initail states
+const initailCakeState = {
+  numOfCake: 10,
+};
+const initialIceCreamState = {
+  numOfIceCream: 20,
+};
+
+// -------------    reducers
+const cakeReducer = (state = initailCakeState, action) => {
+  if (action.type === "BUY_CAKE") {
+    return { ...state, numOfCake: state.numOfCake + 1 };
+  } else {
+    return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  if (action.type === BUY_ICECREAM) {
+    return { ...state, numOfIceCream: state.numOfIceCream + 1 };
+  } else {
+    return state;
+  }
+};
+
+// -------------    store
+const redux = require("redux");
+// ====== LOGGER
+const reduxLogger = require("redux-logger");
+const applyMiddleware = redux.applyMiddleware;
+
+const logger = reduxLogger.createLogger();
+
+//  combine both reducer into one root reducer
+const rootReducer = redux.combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+//  pass rootreducer in create store
+const store = redux.createStore(rootReducer, applyMiddleware(logger));
+
+console.log("intinal State", store.getState());
+const unsubscribe = store.subscribe(() => {
+//   console.log("upadate State", store.getState());
 });
 store.dispatch(buyCake());
 store.dispatch(buyCake());
